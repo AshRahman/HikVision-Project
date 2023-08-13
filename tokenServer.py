f1cf59b0127e6b5becdae507f9bdf4e5885163c9
AshRahman <ashfaqurrahmanrahat@outlook.com>
2023-08-13 17:18:44 +0600

    Unauthintacated data is now shown in console, just need to incorporate it in frontend

--- tokenServer.py
+++ tokenServer.py
@@ -34,12 +34,11 @@ def handle_event():
         personCode = data["params"]["events"][0]["data"][
             "personCode"
         ]  # Person Code required for the PersonInfo API
-        print(personCode)
-
-        # -------------------------------------------------------------
-        ## Below code call the API to find the person information by PersonID
+        print("Personcode is:"+personCode)
+        
+         # -------------------------------------------------------------
+         ## Below code call the API to find the person information by PersonID
         url = "https://127.0.0.1/artemis/api/resource/v1/person/personCode/personInfo"
-
         payload = json.dumps({f"personCode": personCode})
         headers = {
             "x-ca-key": "20942152",
@@ -47,28 +46,24 @@ def handle_event():
             "x-ca-signature-headers": "x-ca-key",
             "Content-Type": "application/json",
         }
-
         response = requests.request(
             "POST", url, headers=headers, data=payload, verify=False
         )
-
         received_data = response.text
+        print("Recieved Data:"+received_data)
         data_final = json.loads(received_data)
         person_id = data_final["data"]["personId"]
         person_code = data_final["data"]["personCode"]
         person_name = data_final["data"]["personName"]
         pic_uri = data_final["data"]["personPhoto"]["picUri"]
-
         # Print the extracted values
         print("Person ID:", person_id)
         print("Person Code:", person_code)
         print("Person Name:", person_name)
         print("Pic URI:", pic_uri)
-
         # ------------------------------------------------
         ## Picture Fetch API
         pic_url = "https://127.0.0.1/artemis/api/resource/v1/person/picture_data"
-
         pic_payload = json.dumps(
             {
                 "personId": person_id,
@@ -81,7 +76,6 @@ def handle_event():
             "x-ca-signature-headers": "x-ca-key",
             "Content-Type": "application/json",
         }
-
         pic_response = requests.request(
             "POST", pic_url, headers=pic_headers, data=pic_payload, verify=False
         )
@@ -89,26 +83,23 @@ def handle_event():
         print(type(pic_response))
         b64pic = pic_response.text  # .split("data:image/jpeg;base64,", 1)[1]
         # ----------------------------------------------------------
-
         # ---------------------------------------------------------
         # Extract the Base64-encoded image data from the string
         data_idx = pic_response.text.find(",") + 1
         image_data = pic_response.text[data_idx:]
-
         # Decode the Base64 string to binary data
         binary_data = base64.b64decode(image_data)
-
         # Save the binary data as a JPEG image
         with open(f"static/images/{person_name}.jpg", "wb") as f:
             f.write(binary_data)
-
         print(f"Image saved as '{person_name}.jpg'")
-
+        
         person_data["data"]["personName"] = person_name
         person_data["data"]["personPhoto"]["picUri"] = b64pic
+        print(person_data["data"]["personName"])
         print(type(person_data))
-        # ---------------------------------------------------------------
-
+            # ---------------------------------------------------------------
+        
         return jsonify({"message": "Event received successfully"}), 200
 
     except Exception as e:
@@ -132,8 +123,11 @@ def generate():
     while True:
         # Send the received_data as an SSE event
         if person_data['data']['personName']:
-            
+            temp=person_data['data']['personName']
             yield f"data: {person_data['data']['personName']}\n\n"
+        else:
+             person_data["data"]["personName"] = "Stranger"
+             yield f"data: {person_data['data']['personName']}\n\n"
         # yield f"output.jpg"
         time.sleep(2)  # Add a delay to avoid overloading the server
 
